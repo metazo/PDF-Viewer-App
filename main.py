12e9f3cacffbae3e12f3786c2dc36e5fc81c9716
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-20 01:10:52 +0200

    Instalizing Tk

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from tkinter import *
 from tkinter import filedialog
+from tkPDFViewer import tkPDFViewer as pdf
+import os
+
+#instalizing tk
+root=Tk()
+root.geometry("630x700+400+100")
+root.title("PDF viewer")
+root.configure(bg="white")
+
+Button(root, text="ouvrir", command=browseFiles, width=40, 
+       font="arial 20", bd=4).pack()
+
+root.mainloop()
