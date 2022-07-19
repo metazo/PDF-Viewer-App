0aca70a46fa36f9b327cd9fa6eda006029b8106b
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-20 01:34:24 +0200

    Opening a file

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ root.geometry("630x700+400+100")
 root.title("PDF viewer")
 root.configure(bg="white")
 
+#main part of program
+def browseFiles():
+    filename=filedialog.askopenfilename(intialdir=os.getcwd(), 
+                                        title="Selectionne un fichier pdf",
+                                        filetype=(("PDF File", ".pdf"),
+                                                  ("PDF File", ".PDF"),
+                                                  ("All files",".txt")))
+
 Button(root, text="ouvrir", command=browseFiles, width=40, 
        font="arial 20", bd=4).pack()
 
